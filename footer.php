c3a097f8cf8902b64c53ba5df48a41e12701407e
Shikha <shikha.parashar_cs18@gla.ac.in>
2020-11-23 22:53:04 +0530

    Footer changed

--- footer.php
+++ footer.php
@@ -1,11 +1,35 @@
-<!-- Footer -->
-<footer class="page-footer font-small blue">
+<!--  Footer -->
+<style>
+	.slinks a:link,
+.slinks a:visited,
+.slinks a:active{
+    color: white;
+}
+.slinks a:hover{
+    text-decoration: none;
+</style>
+            <div class="container-fluid">
+            <div class="d-flex justify-content-between bg-dark px-5 pt-2">
+                <div class="p-2 slinks" id="link">
+                    <span class="mr-5 text-primary font-weight-bolder">FOLLOW US ON</span>
+                    <a href="https://twitter.com/" target="_blank">
+                        TWITTER<i class="fa fa-twitter ml-2" aria-hidden="true"></i>
+                    </a>
+                    <span class="mx-2">|</span>
+                    <a href="https://facebook.com/" target="_blank">
+                        FACEBOOK <i class="fa fa-facebook ml-2" aria-hidden="true"></i>
+                    </a>
+                    <span class="mx-2">|</span>
+                    <a href="https://instagram.com/">
+                        INSTAGRAM <i class="fa fa-instagram" aria-hidden="true"></i>
+                    </a>
+                </div>
 
-  <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2020 Copyright |
-    <a href="index.php">GetTop3</a>
-  </div>
-  <!-- Copyright -->
-
-</footer>
+                <!-- Copyright-->
+                <div class="p-2 slinks text-white">
+                © 2020  <a href="index.php" class="font-weight-bolder text-primary">Get TOP3 </a> All Rights Reserved.  
+                </div>
+                
+            </div>  
+            </div>
 <!-- Footer -->
