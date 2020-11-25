30caa27f29bb17a924179c4b00d9f9b0d918e704
Vishal Sharma <vsunite3@gmail.com>
2020-11-25 09:20:25 +0530

    Updated tws.php

--- tws.php
+++ tws.php
@@ -21,9 +21,9 @@
 
 <br>
     
-  <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-5 justify-content-between">
+  <div class="d-flex justify-content-around">
 
-    <div class="bg-info pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+    <div class="bg-info flex-fill p-2 m-3 text-center overflow-hidden">
       <div><p class="h3">#1</p></div>
       <div class="my-3 p-1">
         <h2 class="display-5">OnePlus Buds</h2>
@@ -33,7 +33,7 @@
       <a href="#" class="btn btn-dark px-5">Full Details <span><i class="fa fa-angle-down" aria-hidden="true"></i></span></a>
     </div>
     <br><br>
-    <div class="bg-info mx-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+    <div class="bg-info flex-fill p-2 text-center overflow-hidden">
       <div><p class="h3">#2</p></div>
       <div class="my-3 p-1">
         <h2 class="display-5">OPPO Enco W51</h2>
@@ -43,7 +43,7 @@
       <a href="#" class="btn btn-dark px-5">Full Details <span><i class="fa fa-angle-down" aria-hidden="true"></i></span></a>
     </div>
     <br><br>
-    <div class="bg-info pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+    <div class="bg-info flex-fill p-2 m-3 text-center overflow-hidden">
       <div><p class="h3">#3</p></div>
       <div class="my-3 p-1">
         <h2 class="display-5">CROSSBEATS Urban</h2>
@@ -52,9 +52,34 @@
       <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 200px; border-radius: 21px 21px 0 0;"><img style="width: 100%; height: inherit;border-radius:inherit;" src="imgs/urban.jpg" alt="Crossbeats"></div>
       <a href="#" class="btn btn-dark px-5">Full Details <span><i class="fa fa-angle-down" aria-hidden="true"></i></span></a>
     </div>
-    <br><br>
+    <br>
 
   </div>
+
+<br><br>
+  <!-- Specs -->
+
+<div class="d-flex">
+  <div class="p-2 bg-light" style="width:40%;height:100%;">
+    <div class="d-flex flex-column">
+      <!-- <div class="p-2 bg-light"><div class="h2 text-center">OnePlus Buds</div></div> -->
+      <div class="p-2 w-100 bg-light">
+        <img src="imgs/oneplus-buds.jpg" class="w-100 h-100" alt="OnePlus">
+      </div>
+      <div class="p-2 bg-light">
+        <div class="d-flex">
+          <div class="flex-fill text-center"><a href="#" class="btn ">Add to Favourites <span><i class="fa fa-heart-o" aria-hidden="true"></i></span></a></div>
+          <div class="flex-fill text-center"><a href="https://www.amazon.in/OnePlus-E501A-Buds-White/dp/B08CVMXPGY/ref=sr_1_2?dchild=1&keywords=oneplus+buds&qid=1606184641&sr=8-2" class="btn ">Buy Now <span><i class="fa fa-shopping-cart" aria-hidden="true"></i></span></a></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="p-2 bg-warning flex-grow-1">
+    <div class="d-flex">
+      <div></div>
+    </div>
+  </div>
+</div>
     
 </body>
 </html>
