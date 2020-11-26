7ba09d2c77ae0414afaa8429996f1dc458b5d376
abhisharmaXD <abhishek.kumar_cs18@gla.ac.in>
2020-11-26 21:20:43 +0530

    add social site link in footer

--- footer.php
+++ footer.php
@@ -12,15 +12,15 @@
             <div class="d-flex justify-content-between bg-dark px-5 pt-2">
                 <div class="p-2 slinks" id="link">
                     <span class="mr-5 text-primary font-weight-bolder">FOLLOW US ON</span>
-                    <a href="https://twitter.com/" target="_blank">
+                    <a href="https://twitter.com/GetTop3" target="_blank">
                         TWITTER<i class="fa fa-twitter ml-2" aria-hidden="true"></i>
                     </a>
                     <span class="mx-2">|</span>
-                    <a href="https://facebook.com/" target="_blank">
+                    <a href="https://www.facebook.com/Get_Top3-103544111601856/?modal=admin_todo_tour" target="_blank">
                         FACEBOOK <i class="fa fa-facebook ml-2" aria-hidden="true"></i>
                     </a>
                     <span class="mx-2">|</span>
-                    <a href="https://instagram.com/">
+                    <a href="https://www.instagram.com/get_top3/" target="_blank">
                         INSTAGRAM <i class="fa fa-instagram" aria-hidden="true"></i>
                     </a>
                 </div>
